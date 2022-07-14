95b0cc431ab2d98a11d01bf563c705db6bcfa635
ahmed khaled ali <58488520+ahmed635@users.noreply.github.com>
2022-07-14 06:10:47 +0200

    Delete urls.py

--- urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('patient-<int:pat_id>', views.patient, name='patient'),
-    path('chart-<int:chart_id>', views.charts, name='charts'),
-    
-]
