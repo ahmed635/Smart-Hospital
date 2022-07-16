e6c295838d221fa6edef5a05aa9529d0fe98807c
mgdym7md <mgdym7md@yahoo.com>
2022-07-16 18:28:17 +0200

    Update settings.py

--- smart_hospital/settings.py
+++ smart_hospital/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 
 Debug = True
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost,https://smarthospitalhelwan.herokuapp.com").split(",")
-
+# ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost,https://smarthospitalhelwan.herokuapp.com").split(",")
+ALLOWED_HOSTS = ["127.0.0.1","localhost","https://smarthospitalhelwan.herokuapp.com"]
 CSRF_TRUSTED_ORIGINS=["https://smarthospitalhelwan.herokuapp.com"]
 
 # Application definition
